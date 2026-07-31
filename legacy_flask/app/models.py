496a0cf6312b83747340e863fc75df5a643a1db7
Arkady Zaytsev <arkan@list.ru>
2026-07-31 16:51:03 +0300

    Move legacy Flask application to legacy_flask

---
+++ legacy_flask/app/models.py
@@ -0,0 +1,10 @@
+from app.database import db
+
+
+class Test(db.Model):
+
+    __tablename__ = "test"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(db.String(100), nullable=False)
