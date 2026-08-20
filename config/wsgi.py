40b4fd5e61c3ac59887a0c1b1f250d54a169ab3b
Arkady Zaytsev <arkan@list.ru>
2026-08-20 14:48:28 +0300

    Load Django settings from environment

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,15 +1,19 @@
 """
 WSGI config for config project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+from pathlib import Path
 
+from dotenv import load_dotenv
 from django.core.wsgi import get_wsgi_application
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(
+    BASE_DIR / ".env"
+)
 
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
